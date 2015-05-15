ecbafa575f62111043f4a33cd488ac0b7038f271
Ramón García <Developer@Developers-Mac-mini.local>
2015-05-15 10:55:06 -0500

    First commit

--- Fibonacci/ViewController.m
+++ Fibonacci/ViewController.m
@@ -14,14 +14,50 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void) viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _mem = [NSMutableDictionary new];
+    
+//    for(int i = 0; i < 100; i++ )
+//        NSLog( @"Is %d Fibonacci? %d", i, [self isFibonacciNumber: i] );
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void) calculateFibonacci
+{
+    int a = 0;
+    int b = 1;
+    int temp = 0;
+    
+    [_mem setObject:@"1" forKey: [NSString stringWithFormat:@"%d", 0]];
+    [_mem setObject:@"1" forKey: [NSString stringWithFormat:@"%d", 1]];
+    
+    for(int i = 0; i < 44; i++ )
+    {
+        temp = a + b;
+        //NSLog( @"%d\n", temp  );
+        [_mem setObject:@"1" forKey: [NSString stringWithFormat:@"%d", temp]];
+        a = b;
+        b = temp;
+    }
+}
+
+-(BOOL) isFibonacciNumber: (int) n
+{
+    static BOOL once = YES;
+    if( once )
+    {
+        [self calculateFibonacci];
+        once = NO;
+    }
+    
+    return [_mem objectForKey: [NSString stringWithFormat:@"%d", n]] != nil;
 }
 
 @end
